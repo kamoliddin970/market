b2558c25aad49640e5c1d4f1515d1534759fd498
bilolbackend80 <bsobirdjonov80@gmail.com>
2024-11-01 00:26:50 +0500

    Updatebilol.js

--- bilol.js
+++ bilol.js
@@ -1,1 +1,27 @@
 console.log("Bilol");
+class Employee {
+  getSalary() {
+    console.log("Xodimning maoshi aniqlandi.");
+  }
+}
+
+class Manager extends Employee {
+  getSalary() {
+    console.log("Menejerning maoshi: 5000$");
+  }
+}
+
+class Developer extends Employee {
+  getSalary() {
+    console.log("Dasturchining maoshi: 4000$");
+  }
+}
+
+class Designer extends Employee {
+  getSalary() {
+    console.log("Dizaynerning maoshi: 3000$");
+  }
+}
+
+let xodimlar = [new Manager(), new Developer(), new Designer()];
+xodimlar.forEach((xodim) => xodim.getSalary());
