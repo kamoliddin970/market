1f89362bc36813c66038c7d2fbba6526ed3d8d9c
javohir2303 <javohiryakubov49@gmail.com>
2024-11-01 20:44:23 +0500

    o'zgartrildi kod

--- index.js
+++ index.js
@@ -64,15 +64,18 @@ console.log(balcon.getTicket());
 
 // javohir
 
-const arr = []
-function javohir(){
-    arr.push(javohir)
-    console.log(arr.length)
+const javohirCloses = ()=>{
+  let i = 1
+  return ()=>{
+    return i++
+  }
 }
-javohir()
-javohir()
-javohir()
-javohir()
-javohir()
-javohir()
+const javohir = javohirCloses()
+console.log(javohir())
+console.log(javohir())
+console.log(javohir())
+console.log(javohir())
+console.log(javohir())
+console.log(javohir())
+console.log(javohir())
 
