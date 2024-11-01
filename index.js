297dcc21b96b3ee72ad7cf2437841c5983662d25
khamidov-online <khamidovonline@gmail.com>
2024-11-01 20:50:35 +0300

    closure

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 console.log("ok");
 console.log(7 + 7);
 console.log("Bekzod");
+
+
+
+
+
+const Bekmuhammad = ()=>{
+    i=0
+    return ()=>{
+        return ++i
+    }
+}
+const chaqiruv = Bekmuhammad();
+console.log(chaqiruv());
