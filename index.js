6003a60530de4d564eb46c4569aa5b44af7a9cc5
kamoliddin571 <kamoliddinjamoliddinov571@gmail.com>
2024-11-01 19:30:33 +0500

    i cut from Husniddin's codes

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
 console.log("ok");
 console.log(7 + 7);
-console.log("Bekzod");
+
+// 1-misol
+
+class Hayvon {
+  speak() {
+    console.log("Hayvon ovoz chiqardi.");
+  }
+}
+
+class dog extends Hayvon {
+  speak() {
+    console.log("Vov-vov!");
+  }
+}
+
+class cat extends Hayvon {
+  speak() {
+    console.log("Miyov!");
+  }
+}
+
+const it = new dog();
+const mushuk = new cat();
+
+it.speak();
+mushuk.speak();
+
+// 2-misol
+
+class Ticket {
+  getTicket() {
+    return 500000;
+  }
+}
+class oldiQator extends Ticket {}
+class ortaQator extends Ticket {
+  getTicket() {
+    return 300000;
+  }
+}
+class Balcon extends Ticket {
+  getTicket() {
+    return 100000;
+  }
+}
+const oldi = new oldiQator();
+const orta = new ortaQator();
+const balcon = new Balcon();
+
+console.log(oldi.getTicket());
+console.log(orta.getTicket());
+console.log(balcon.getTicket());
