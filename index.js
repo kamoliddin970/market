7216527af935de76081eab8cdf94ec4d4861f4da
Husniddin001 <you@example.com>
2024-11-01 20:02:26 +0500

    function

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log("ok");
 console.log(7 + 7);
 console.log("Bekzod");
+
+function husniddin() {
+    return (i){
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            
+        }
+    }
+    
+}
+console.log(husniddin());
